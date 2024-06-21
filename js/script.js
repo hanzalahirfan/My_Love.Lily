33168dd462619d35c278d860c3b63609a353b4cc
Muhammad Hanzalah <108806629+hanzalahirfan@users.noreply.github.com>
2024-06-21 14:37:41 +0500

    commited

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+<section id="poetry" class="container mt-5">
+    <h2 class="text-center">Poetry</h2>
+    <div class="row">
+  
+        <div class="col-md-4 mb-4">
+            <div class="poetry-box shadow">
+                <p class="poetry-text">Poetry 1 content...</p>
+            </div>
+        </div>
+
+        <div class="col-md-4 mb-4">
+            <div class="poetry-box shadow">
+                <p class="poetry-text">Poetry 2 content...</p>
+            </div>
+        </div>
+  
+        <div class="col-md-4 mb-4">
+            <div class="poetry-box shadow">
+                <p class="poetry-text">Poetry 3 content...</p>
+            </div>
+        </div>
+
+        <div class="col-md-4 mb-4">
+            <div class="poetry-box shadow">
+                <p class="poetry-text">Poetry 4 content...</p>
+            </div>
+        </div>
+    </div>
+</section>
